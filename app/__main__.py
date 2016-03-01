0e6dd2a1799484cb40b6214eeafce57993e1276a
ihor-pyvovarnyk <igor.pivovarnik@gmail.com>
2016-03-02 01:13:56 +0200

    Added virtualenv check

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,12 @@
+# import numpy
+import sys
+
 from app import Application
 import config
+
+# Check is into virtual env
+if not hasattr(sys, 'real_prefix'):
+    raise Exception('Need to run into virtual environment. Run source bin/activate for activate virtualenv')
 
 if __name__ == '__main__':
     Application.run(config)
